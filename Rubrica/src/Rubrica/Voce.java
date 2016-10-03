1a54948bc62394b2d7cd6f6463f82f135ba2a43b
marti90 <debernardi.martina@gmail.com>
2016-10-03 12:06:14 +0200

    Inizio progetto Rubrica

---
+++ Rubrica/src/Rubrica/Voce.java
@@ -0,0 +1,48 @@
+package Rubrica;
+
+public class Voce {
+	
+	private String nome="";
+	private String cognome="";
+	private String tel="";
+	
+	public Voce(){
+		
+	}
+	
+	public Voce(String nome, String cognome, String tel) {
+		
+		this.nome = nome;
+		this.cognome = cognome;
+		this.tel = tel;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	
+	
+	
+
+}
---
+++ Rubrica/src/Rubrica/VoceGiàEsiste.java
@@ -0,0 +1,14 @@
+package Rubrica;
+
+public class VoceGi‡Esiste extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public VoceGi‡Esiste(String s){
+		super(s);
+	}
+
+}
